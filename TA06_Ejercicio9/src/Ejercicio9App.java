df7b0db213978d430fbb976a449e4be5db80565f
jmiguel.suarez <jmiguel@automaticaintegral.com>
2022-07-11 16:00:35 +0200

    Ejercicio 9

---
+++ TA06_Ejercicio9/src/Ejercicio9App.java
@@ -0,0 +1,37 @@
+import javax.swing.JOptionPane;
+
+
+public class Ejercicio9App {
+
+	public static void main(String[] args) {
+		
+		generarNumeros();
+		mostrarNumeros();
+	}
+    
+	public static double[] generarNumeros(){ 
+	// metodo para llenar arreglo de numeros
+		String tamano = JOptionPane.showInputDialog("Introduce el tamaño del array");
+		int tamanoInt = Integer.parseInt(tamano);
+		
+		double crear [] = new double [tamanoInt];
+		for(int i=0; i < tamanoInt ; i++){
+	    	String inicio = JOptionPane.showInputDialog("Introduce un numero");
+			double inicioConvert = Double.parseDouble(inicio);
+			crear [i] = inicioConvert;
+		}
+			return crear;	
+    }
+	
+	public static void mostrarNumeros(){ 
+	// metodo para mostrar la posicion del arreglo y el numero guardado en esa posicion
+		
+		double listar [] = generarNumeros();
+				
+		for(int i=0; i < listar.length ; i++){
+			
+			JOptionPane.showMessageDialog(null, "Los numeros son:"+i+listar[i]);
+		}	
+    }
+}
+
