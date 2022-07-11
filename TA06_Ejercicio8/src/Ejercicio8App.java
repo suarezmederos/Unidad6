7e39e2f57ba8b9a96fe91f73b9607d4d54463363
jmiguel.suarez <jmiguel@automaticaintegral.com>
2022-07-11 15:59:14 +0200

    Ejercicio 8

---
+++ TA06_Ejercicio8/src/Ejercicio8App.java
@@ -0,0 +1,37 @@
+
+import javax.swing.JOptionPane;
+
+
+public class Ejercicio8App {
+
+	public static void main(String[] args) {
+		
+		generarNumeros();
+		mostrarNumeros();
+	}
+    
+	public static double[] generarNumeros(){ 
+	// metodo para llenar arreglo de numeros
+		
+		int tamano = 10;
+		double crear [] = new double [tamano];
+		
+		for(int i=0; i < tamano ; i++){
+	    	String inicio = JOptionPane.showInputDialog("Introduce un numero");
+			double inicioConvert = Double.parseDouble(inicio);
+			crear [i] = inicioConvert;
+		}
+			return crear;	
+    }
+	
+	public static void mostrarNumeros(){ 
+	// metodo para mostrar la posicion del arreglo y el numero guardado en esa posicion
+		
+		double listar [] = generarNumeros();
+				
+		for(int i=0; i < listar.length ; i++){
+			
+			JOptionPane.showMessageDialog(null, "Los numeros son:"+i+listar[i]);
+		}	
+    }
+}
